0995ba6443591b420e162518399d76e9b65b1363
Thiago Souza <thiagossouza07@gmail.com>
2022-03-01 23:36:16 -0300

    Acesso Cep requisitando API

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from Datas_Br import DatasBr
+from acesso_cep import BuscaEndereco
 import re
 
-cadastro = DatasBr()
+cep = 49500217
+objeto_cep = BuscaEndereco(cep)
+print(objeto_cep)
 
-print(cadastro)
-print(cadastro.tempo_cadastro())
+logradouro, bairro, localidade, uf = objeto_cep.acessa_via_cep()
+print(logradouro+",", bairro,"-", localidade + "/" + uf)
+
+
 
